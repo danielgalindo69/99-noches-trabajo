c25e69ac45da94bc9d0b46af2fd22773b5aa36bc
daniel galindo <danielmauriciogalindo07@gmail.com>
2025-09-01 11:36:03 -0500

    interfaces

--- abstraccion/main.java
+++ abstraccion/main.java
@@ -1,43 +1,85 @@
 
+import java.util.Scanner;
+
+interface Anima {
+    void caminar();
+    void dormir();
+}
+
 abstract class Animales {
     public String raza;
     public String edad;
     public String nombre;
-    
-    Animales(String raza, String edad, String nombre){
+
+    Animales(String raza, String edad, String nombre) {
         this.raza = raza;
         this.edad = edad;
         this.nombre = nombre;
     }
 
-    abstract void gemir();
+    abstract void comer();
 }
 
-class caballo extends Animales {
-    caballo(String raza, String edad, String nombre){
+class Perro extends Animales implements Anima {
+    Perro(String raza, String edad, String nombre) {
         super(raza, edad, nombre);
     }
 
-    void gemir(){
-        System.out.println("el perro gime de la alegria");
+    void comer() {
+        System.out.println(nombre + " el perro come. Tiene " + edad + " años y es de raza " + raza + ".");
+    }
+
+    public void dormir(){
+        System.out.println("Esta dormido");
+    } 
+    
+    public void caminar(){
+        System.out.println("Esta dormido");
     }
 }
 
 class Gato extends Animales {
-    Gato(String raza, String edad, String nombre){
+    Gato(String raza, String edad, String nombre) {
         super(raza, edad, nombre);
     }
 
-    void gemir(){
-        System.out.println("el gato gime de la alegria");
+    void comer() {
+        System.out.println(nombre + " el gato come. Tiene " + edad + " años y es de raza " + raza + ".");
     }
 }
 
-public class main  {    
+public class main {
     public static void main(String[] args) {
-        caballo jordi = new caballo("p", "70", "pepe");
-        Gato p = new Gato("g", "40", "t");
-        jordi.gemir();
-        p.gemir();
+        Scanner scanner = new Scanner(System.in);
+       
+        System.out.println("Ingrese los datos del perro:");
+        System.out.print("Raza: ");
+        String razaPerro = scanner.nextLine();
+        System.out.print("Edad: ");
+        String edadPerro = scanner.nextLine();
+        System.out.print("Nombre: ");
+        String nombrePerro = scanner.nextLine();
+
+        Perro perro = new Perro(razaPerro, edadPerro, nombrePerro);
+
+      
+        System.out.println("\nIngrese los datos del gato:");
+        System.out.print("Raza: ");
+        String razaGato = scanner.nextLine();
+        System.out.print("Edad: ");
+        String edadGato = scanner.nextLine();
+        System.out.print("Nombre: ");
+        String nombreGato = scanner.nextLine();
+
+        Gato gato = new Gato(razaGato, edadGato, nombreGato);
+
+      
+        System.out.println("Comportamiento de los animales:");
+        perro.comer();
+        gato.comer();
+        perro.dormir();
+
+        scanner.close();
+
     }
 }
